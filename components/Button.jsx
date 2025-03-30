b5d2d7cebbe521b481fe3effc8cc84bf8b0536e6
amaechinwankwo <damaechi@rocketmail.com>
2025-03-30 09:17:31 +0100

    my mobile commit

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ children }) {
   return (
     <div>
-      <button className="rounded-2xl bg-gradient-to-r from-[#C28383] to-[#7C2CC2] font-[400] text-[20px] py-2 text-white font-marcellus-SC w-full">
+      <button className="rounded-2xl bg-gradient-to-r from-[#C28383] to-[#7C2CC2] hover:from-[#7C2CC2] hover:to-[#0697DF] font-[400] text-[20px] py-2 text-white font-marcellus-SC w-full">
         {children}
       </button>
     </div>
--- components/Footer2.jsx
+++ components/Footer2.jsx
@@ -27,7 +27,7 @@ export default function Footer2() {
                 rows={5}
               />
 
-              <button className="rounded-4xl bg-gradient-to-r from-[#C28383] to-[#7C2CC2] font-[400] text-[20px] py-3 text-white font-marcellus-SC w-full">
+              <button className="rounded-4xl bg-gradient-to-r from-[#C28383] to-[#7C2CC2] hover:from-[#7C2CC2] hover:to-[#0697DF] font-[400] text-[20px] py-3 text-white font-marcellus-SC w-full">
                 Submit
               </button>
             </form>
